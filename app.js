b16a2ab93c8ee2f6ae7aecccfce74ab14cffebcb
joshua S Park <jiyong11281@gmail.com>
2018-01-15 18:55:12 -0500

    'twitter-js'

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require( 'express' );
+const app = express();
+const volleyball = require('volleyball')
+
+app.use(volleyball);
+
+
+app.get('/',(req,res) => res.send('hello world'))
+
+app.use('/deep/new', (req, res) => res.send('this is deep area'))
+
+app.use(function(req, res, next) {
+  console.log(req.method)
+  next()
+})
+app.post('/', function(req, res) {
+  res.send('Post')
+  // next()
+})
+
+app.listen(3000)
