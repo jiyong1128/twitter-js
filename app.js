b6fdaeaca3e8780924cf37fb76c8635d2ba0c263
Domnik Mack <domnikmack@gmail.com>
2018-01-16 12:56:10 -0500

    installed lodash

--- app.js
+++ app.js
@@ -5,18 +5,29 @@ const nunjucks = require('nunjucks');
 
 console.log('hello wolrd');
 
-var items = {{name: 'Gandolph'}};
+var items = {
+  title: 'sample',
+  people : [
+    {name: 'Gandolf'},
+    {name: 'Frodo'},
+    {name: 'Hermione'},
+  ],
+  hello: false
+};
 
-
-nunjucks.configure('views');
-var response = nunjucks.render('index.html', items);
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {noCache: true});
+//var response = nunjucks.render('index.html', items);
 
 app.use(volleyball);
 
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
-app.get('/',(req,res) => res.send(response));
+app.get('/',(req,res) => res.render('index', {title: 'Hall of Fame', people: people}));
+app.get('/items',(req,res) => res.render('index', items));
 
-app.use('/deep/new', (req, res) => res.send('this is deep area'))
+app.use('/deep/new', (req, res) => res.send('this is deep area'));
 
 app.use(function(req, res, next) {
   console.log(req.method)
